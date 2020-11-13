e0cf55400cf2fa4d2421a498aa5327cd9cdc5169
Mukesh Kumar <mukeshkmr776@gmail.com>
2020-11-14 01:33:12 +0530

    updated position

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const fs = require('fs');
 const path = require('path');
 
+function getStats(path) {
+    return fs.statSync(path);
+}
+
+function getCreatedDate(date) {
+    return (date.toISOString().split('T'))[0]
+}
+
+function getFileMetadata(file, name, pathName) {
+    console.log(name, pathName);
+    return {
+        "fileName" : name, //name of the file
+        "filePath" : path.relative(__dirname, path.join(pathName, name)), //path to the file relative to the root of the project
+        "size" : file.size, //size of the file in bytes
+        "createdAt" : getCreatedDate(file.birthtime), // date when the file was created in the format YYYY-MM-DD. The date should be normalized to UTC.
+        "isDirectory" : file.isDirectory() // whether the path is a directory (Boolean)
+      }
+}
+
 const main = (dirOrFilePath) => {
     return new Promise((resolve, reject) => {
         if(!fs.existsSync(dirOrFilePath)) {
@@ -23,26 +42,6 @@ const main = (dirOrFilePath) => {
         resolve(result);
     })
 };
-
-
-function getStats(path) {
-    return fs.statSync(path);
-}
-
-function getCreatedDate(date) {
-    return (date.toISOString().split('T'))[0]
-}
-
-function getFileMetadata(file, name, pathName) {
-    console.log(name, pathName);
-    return {
-        "fileName" : name, //name of the file
-        "filePath" : path.relative(__dirname, path.join(pathName, name)), //path to the file relative to the root of the project
-        "size" : file.size, //size of the file in bytes
-        "createdAt" : getCreatedDate(file.birthtime), // date when the file was created in the format YYYY-MM-DD. The date should be normalized to UTC.
-        "isDirectory" : file.isDirectory() // whether the path is a directory (Boolean)
-      }
-}
 
 
 module.exports = main;
